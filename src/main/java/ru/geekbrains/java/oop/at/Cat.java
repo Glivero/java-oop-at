3bba0061c9a08ebee1b92dc5e6c970e3f279cef3
Denis <denisermakov1986@gmail.com>
2020-07-17 10:47:03 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/java/oop/at/Cat.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.java.oop.at;
+
+public class Cat {
+    String name;
+
+    public void eat(CatFood catFood){
+        System.out.println(name + " кушает " + catFood.getProductName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/geekbrains/java/oop/at/CatFood.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.java.oop.at;
+
+public class CatFood {
+    private String productName;
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+}
